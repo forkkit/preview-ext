e045a0cb3dde5502f0132e878949f3feab66bced
Rob Loranger <dev@loranger.xyz>
2019-10-11 09:56:10 -0700

    initial commit
    working in firfox 69 and chrome(ium) 77

---
+++ background.js
@@ -0,0 +1,110 @@
+var currentTab;
+function updateIcon(previewing) {
+	chrome.browserAction.setIcon({
+		path: previewing ? {
+			19: "/icons/black-bg.svg",
+			38: "/icons/black-bg.svg"
+		} : {
+			19: "/icons/white-bg.svg",
+			38: "/icons/white-bg.svg"
+		},
+		tabId: currentTab.id
+	});
+	chrome.browserAction.setTitle({
+		title: previewing ? 'Hide Preview' : 'Show Preview',
+		tabId: currentTab.id
+	});
+}
+
+function togglePreview() {
+	// tell content to render preview
+	chrome.tabs.sendMessage(currentTab.id, {preview: true}, (result) => {
+		// if content responds with preview being active
+		// clear the preview instead
+		if (result && result.preview) {
+			chrome.tabs.sendMessage(currentTab.id, {preview: false});
+		}
+		updateIcon(!result.preview);
+	});
+}
+
+// listen for clicks on browser extension icon
+chrome.browserAction.onClicked.addListener(togglePreview);
+
+function disableExtension() {
+	// when the content script reports back that there
+	// are no supported text areas we should disable action
+	chrome.browserAction.disable(currentTab.id);
+	chrome.browserAction.setTitle({
+		title: 'No Preview Available',
+		tabId: currentTab.id
+	});
+}
+
+function updateActiveTab() {
+	// don't try to inject the script on pages that
+	// are not webpages, i.e. chrome:// or about://
+	function isSupportedProtocol(urlString) {
+		var supportedProtocols = ["https:", "http:", "file:"];
+		var url = document.createElement('a');
+		url.href = urlString;
+		return supportedProtocols.indexOf(url.protocol) != -1;
+	}
+
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+		if (tabs[0]) {
+			currentTab = tabs[0];
+			if (isSupportedProtocol(currentTab.url)) {
+				// check state first, if error execute script?
+				chrome.tabs.sendMessage(currentTab.id, {state: true}, (result) => {
+					if (chrome.runtime.lastError) {
+						console.log(chrome.runtime.lastError.message);
+						disableExtension();
+						console.log(`Could not detect previewable content.`);
+					} else if (result && result.supported) {
+						// when there is supported content we should enable action
+						// and update the icon state, which also updates the tooltip
+						chrome.browserAction.enable(currentTab.id);
+						updateIcon(result.preview);
+					} else {
+						disableExtension();
+						console.log(`No previewable content detected at '${currentTab.url}'`);
+					}
+				});
+			} else {
+				console.log(`Markdown Preview does not support the '${currentTab.url}' URL.`);
+				disableExtension();
+			}
+		}
+	});
+}
+
+
+// update the extension icon for any tab, when switching and updating
+chrome.tabs.onCreated.addListener(updateActiveTab);
+chrome.tabs.onUpdated.addListener(updateActiveTab);
+chrome.tabs.onActivated.addListener(updateActiveTab);
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.endpoint && request.data) {
+		var params = {
+			body: JSON.stringify(request.data),
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		};
+		fetch(request.endpoint, params).then(function(response) {
+			return response.text().then(function(text) {
+				sendResponse([{
+					body: text,
+					status: response.status,
+					statusText: response.statusText,
+				}, null]);
+			});
+		}, function(error) {
+			sendResponse([null, error]);
+		});
+	}
+	return true;
+});
---
+++ content.js
@@ -0,0 +1,88 @@
+(function() {
+	var pad = document.querySelector("textarea#writer");
+	var previewOverlay = document.createElement("DIV");
+	previewOverlay.id = "md-preview";
+	previewOverlay.style.cssText = "position: absolute; top: 0; left: 0; z-index: 20; width: 100%; min-height: 100vh; background-color: #fff;";
+	var previewArticle = document.createElement("ARTICLE");
+	previewArticle.id ="post-body";
+	previewArticle.classList = ['norm'];
+	previewOverlay.appendChild(previewArticle);
+
+	async function showPreview() {
+		var rawPost = getRawPost();
+		var result = await getPostMarkdown(rawPost)
+			.then((res) => {
+				return res.json();
+			});
+		previewArticle.innerHTML = result.body;
+		document.body.appendChild(previewOverlay);
+		document.body.id = "post";
+		pad.style.display = "none";
+	}
+
+	function getRawPost() {
+		// TODO: add other possible, i.e. submit.as
+		// writefreely pad
+		// body#pad textarea#writer
+		return pad.value;
+	}
+
+	function getPostMarkdown(rawpost) {
+		const endpoint = "http://localhost:8080/api/generate/markdownify";
+		var data = {
+			base_url: '',
+			raw_body: rawpost
+		};
+		return new Promise((resolve, reject) => {
+			chrome.runtime.sendMessage({endpoint, data}, messageResponse => {
+				const [response, error] = messageResponse;
+				if (response === null) {
+					reject(error);
+				} else {
+					// Use undefined on a 204 - No Content
+					const body = response.body ? new Blob([response.body]) : undefined;
+					resolve(new Response(body, {
+						status: response.status,
+						statusText: response.statusText,
+					}));
+				}
+			});
+		});
+	}
+
+	function clearPreview() {
+		document.body.id = "pad";
+		pad.style.display = "block";
+		document.body.removeChild(previewOverlay);
+	}
+
+	function isPreview() {
+		return document.body.contains(previewOverlay);
+	}
+
+	function isSupported() {
+		return (document.querySelector("body#pad textarea#writer") !== null);
+	}
+
+	// TODO: sender is never used
+	chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+		if (msg.preview === true) {
+			if (isPreview()) {
+				sendResponse({preview: true});
+			} else {
+				// we send false because when this message was sent,
+				// the preview was not active
+				showPreview();
+				sendResponse({preview: false});
+			}
+		} else if (msg.preview === false) {
+			clearPreview();
+			sendResponse({preview: false});
+		} else if (msg.state === true) {
+			sendResponse({supported: isSupported(), preview: isPreview()});
+		} else {
+			sendResponse({preview: false});
+		}
+	});
+})();
+
