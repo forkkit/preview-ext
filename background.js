d808e23dc9ff7145964ba02edb789aaec043e8d0
Rob Loranger <dev@loranger.xyz>
2019-12-20 13:57:04 -0800

    WIP

--- background.js
+++ background.js
@@ -1,5 +1,53 @@
-var currentTab;
-function updateIcon(previewing) {
+// listen for clicks on browser extension icon
+chrome.browserAction.onClicked.addListener((tab) => {
+	withPermission(tab, togglePreview);
+});
+
+// update the extension icon for any tab, when switching and updating
+chrome.tabs.onCreated.addListener((tab) => {
+	withPermission(tab, updateActiveTab);
+});
+chrome.tabs.onUpdated.addListener((tab) => {
+	withPermission(tab, updateActiveTab);
+});
+chrome.tabs.onActivated.addListener((tab) => {
+	withPermission(tab, updateActiveTab);
+});
+
+chrome.windows.onFocusChanged.addListener((tab) => {
+	withPermission(tab, updateActiveTab);
+});
+
+
+// listen for request from the client script to toggle
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	if (request.toggle) {
+		togglePreview(sender.tab);
+	}
+});
+
+function withPermission(tab, f) {
+	if (tab.url !== undefined) {
+		chrome.permissions.request({
+			permissions: ['tabs'],
+			origins: [tab.url]
+		},(granted) => {
+			if (granted) {
+				chrome.tabs.sendMessage(tab.id, {state: true}, () => {
+					if (chrome.runtime.lastError) {
+						chrome.tabs.executeScript({
+							file: "content.js"
+						});
+					} else {
+						f(tab);
+					}
+				});
+			}
+		});
+	}
+};
+
+function updateIcon(tab, previewing) {
 	chrome.browserAction.setIcon({
 		path: previewing ? {
 			19: "/icons/black-bg.svg",
@@ -8,89 +56,56 @@ function updateIcon(previewing) {
 			19: "/icons/white-bg.svg",
 			38: "/icons/white-bg.svg"
 		},
-		tabId: currentTab.id
+		tabId: tab.id
 	});
 	chrome.browserAction.setTitle({
 		title: previewing ? 'Hide Preview' : 'Show Preview',
-		tabId: currentTab.id
+		tabId: tab.id
 	});
 }
 
-function togglePreview() {
+function togglePreview(tab) {
 	// tell content to render preview
-	chrome.tabs.sendMessage(currentTab.id, {preview: true}, (result) => {
+	chrome.tabs.sendMessage(tab.id, {preview: true}, (result) => {
 		// if content responds with preview being active
 		// clear the preview instead
 		if (result && result.preview) {
-			chrome.tabs.sendMessage(currentTab.id, {preview: false});
-			updateIcon(false);
+			chrome.tabs.sendMessage(tab.id, {preview: false});
+			updateIcon(tab, false);
 		} else if (result && !result.preview) {
-			updateIcon(true);
+			updateIcon(tab, true);
 		}
 	});
 }
 
-// listen for clicks on browser extension icon
-chrome.browserAction.onClicked.addListener(togglePreview);
 
-function disableExtension() {
-	// when the content script reports back that there
-	// are no supported text areas we should disable action
-	chrome.browserAction.disable(currentTab.id);
+function disableExtension(tab) {
+	chrome.browserAction.disable(tab.id);
 	chrome.browserAction.setTitle({
 		title: 'No Preview Available',
-		tabId: currentTab.id
+		tabId: tab.id
 	});
 }
 
-function updateActiveTab() {
-	// don't try to inject the script on pages that
-	// are not webpages, i.e. chrome:// or about://
-	function isSupportedProtocol(urlString) {
-		var supportedProtocols = ["https:", "http:", "file:"];
-		var url = document.createElement('a');
-		url.href = urlString;
-		return supportedProtocols.indexOf(url.protocol) != -1;
-	}
-
-	chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-		if (tabs[0]) {
-			currentTab = tabs[0];
-			if (isSupportedProtocol(currentTab.url)) {
-				// check state first, if error execute script?
-				chrome.tabs.sendMessage(currentTab.id, {state: true}, (result) => {
-					if (chrome.runtime.lastError) {
-						console.log(chrome.runtime.lastError.message);
-						disableExtension();
-						console.log(`Could not detect previewable content.`);
-					} else if (result && result.supported) {
-						// when there is supported content we should enable action
-						// and update the icon state, which also updates the tooltip
-						chrome.browserAction.enable(currentTab.id);
-						updateIcon(result.preview);
-					} else {
-						disableExtension();
-						console.log(`No previewable content detected at '${currentTab.url}'`);
-					}
-				});
-			} else {
-				console.log(`Markdown Preview does not support the '${currentTab.url}' URL.`);
-				disableExtension();
-			}
+function updateActiveTab(tab) {
+	chrome.tabs.sendMessage(tab.id, {state: true}, (result) => {
+		if (chrome.runtime.lastError) {
+			console.log(chrome.runtime.lastError.message);
+			disableExtension(tab);
+			console.log(`Could not detect previewable content.`);
+		} else if (result && result.supported) {
+			// when there is supported content we should enable action
+			// and update the icon state, which also updates the tooltip
+			chrome.browserAction.enable(tab.id);
+			updateIcon(tab, result.preview);
+		} else {
+			disableExtension(tab);
+			console.log(`No previewable content detected at '${tab.url}'`);
 		}
 	});
 }
 
-
-// update the extension icon for any tab, when switching and updating
-chrome.tabs.onCreated.addListener(updateActiveTab);
-chrome.tabs.onUpdated.addListener(updateActiveTab);
-chrome.tabs.onActivated.addListener(updateActiveTab);
-
-chrome.windows.onFocusChanged.addListener(() => {
-	updateActiveTab();
-});
-
+// listen for requests for markdown from the client script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 	if (request.endpoint && request.data) {
 		var params = {
@@ -113,10 +128,4 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 		});
 	}
 	return true;
-});
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-	if (request.toggle) {
-		togglePreview();
-	}
 });
